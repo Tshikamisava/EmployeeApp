30e472cc30772fa880b38d01aff5c5e535fd30ed
Tshikamisava <lhlongwane81@gmail.com>
2023-06-13 15:59:21 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home';
+import Navbar from './Pages/Navbar';
+import Add from './Pages/user/Add';
+import Edit from './Pages/user/Edit';
+import Users from './Pages/user/Users';
+//import React, { Pages } from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Routes>
+        <Route  path="/" exact element={<Home/>} />
+        <Route  path="/home" exact element={<Home/>} />
+        <Route  path="/users/:id" exact element={<Users/>} />
+        <Route  path="/add-user" exact element={<Add/>} />
+        <Route  path="/edit-user/:id" exact element={<Edit/>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
